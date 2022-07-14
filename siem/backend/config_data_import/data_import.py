62695cd9844baca57da3867eafa70cda357e3ab0
ge0k0 <you@example.com>
2022-07-14 23:00:01 +0200

    Data import recreation

--- siem/backend/config_data_import/data_import.py
+++ siem/backend/config_data_import/data_import.py
@@ -8,75 +8,77 @@ from django.db.models import Exists
 # Data import is created to simplify import and migration of Use Case Rules, Groups, Enrichments and Correlations.
 # The User can import only relevant cases in to the system and use templates to create his own searches.
 class Data_Import:
-    def __init__(self):
-        self.available_config_files = os.listdir(current_dir)
+    def open_file(self, filename=None):
+        if filename in self.available_config_files:
+            full_filename = os.path.join(current_dir, filename)
+            with open(full_filename, 'r') as file:
+                config_file_content = yaml.safe_load(file)
+        return config_file_content
 
-        self.available_ucrs = []
-        self.available_groups = []
-        self.available_enrichments = []
-        self.available_correlations = []
-
-        for config_file in self.available_config_files:
-            if config_file.startswith("UCR_"):
-                self.available_ucrs.append(config_file)
-            elif config_file.startswith("GROUP_"):
-                self.available_groups.append(config_file)
-            elif config_file.startswith("ENRICHMENT_"):
-                self.available_enrichments.append(config_file)
-            elif config_file.startswith("CORRELATION_"):
-                self.available_correlations.append(config_file)
-
-
-    def __import_group_into_db(self):
-        if self.filename in self.available_groups:
-            Group.objects.update_or_create(**self.yaml_object)
+    def __import_group_into_db(self, filename=None):
+        if filename in self.available_groups:
+            config_file_content = self.open_file(filename)
+            created_or_updated_group, created_group_boolean = Group.objects.update_or_create(**config_file_content)
         else:
             pass
 
-    def __import_ucr_into_db(self):
-        # Check if the Group exists, since Group dependency is mandatory
-        self.groupObj = Group.objects.filter(Exists(Group.objects.filter(title=self.yaml_object["group"])))
-        if self.groupObj.count() == 0:
-            self.__import_group_into_db(self.filename)
+        return created_or_updated_group, created_group_boolean
 
-        self.groupObj = Group.objects.get(title=self.yaml_object["group"])
-        self.yaml_object["group"] = self.groupObj
-        
-        # Use update_or_create() method, since Object might already exist
-        if hasattr(self, 'groupObj'):
-            UCR.objects.update_or_create(**self.yaml_object)
+    def __import_ucr_into_db(self, filename=None):
+        if filename in self.available_ucrs:
+            config_file_content = self.open_file(filename)
+            
+            # Check if the group dependency is satisfied:
+            # Check if there are more then one group listed
+
+            groups = Group.objects.filter(Exists(Group.objects.filter(title=config_file_content["group"])))
+            groups_count = groups.count()
+            if groups_count == 0:
+                created_group = self.__import_group_into_db(filename=self.available_groups_title_filename_dict[config_file_content["group"]])
+                config_file_content["group"] = created_group
+            elif groups_count == 1:
+                config_file_content["group"] = groups[0]
+            else:
+                pass 
+
+            created_or_updated_ucr, created_ucr_boolean = UCR.objects.update_or_create(**config_file_content)
         else:
             pass
 
-    def __import_enrichment_into_db(self):
-        Enrichment.objects.update_or_create(**self.yaml_object)
+        return created_or_updated_ucr, created_ucr_boolean
+
+    def __import_enrichment_into_db(self, filename=None):
+        if filename in self.available_enrichments:
+            config_file_content = self.open_file(filename)
+            created_or_updated_enrichment, created_enrichment_boolean = Enrichment.objects.update_or_create(**config_file_content)
+        else:
+            pass
+
+        return created_or_updated_enrichment, created_enrichment_boolean
         
-    def __import_correlation_into_db(self):
-        Correlation.objects.update_or_create(**self.yaml_object)
-   
+    def __import_correlation_into_db(self, filename=None):
+        if filename in self.available_correlations:
+            config_file_content = self.open_file(filename)
+            created_or_updated_correlation, created_correlation_boolean = Correlation.objects.update_or_create(**config_file_content)
+        else:
+            pass
+
+        return created_or_updated_correlation, created_correlation_boolean
+
     # Function imports .yml files from the same directory into Django DB
     def import_file(self, filename=None):
-        self.filename = filename
-        if self.filename in self.available_config_files:
-            full_filename = os.path.join(current_dir, self.filename)
-            with open(full_filename, 'r') as file:
-                self.yaml_object = yaml.safe_load(file)
+        if filename in self.available_groups:
+            self.__import_group_into_db(filename)
 
-            if self.filename in self.available_groups:
-                self.__import_group_into_db()
+        if filename in self.available_ucrs:
+            self.__import_ucr_into_db(filename)
 
-            if self.filename in self.available_ucrs:
-                self.__import_ucr_into_db()
+        if filename in self.available_enrichments:
+            self.__import_enrichment_into_db(filename)
+    
+        if filename in self.available_correlations:
+            self.__import_correlation_into_db(filename)
 
-            if self.filename in self.available_enrichments:
-                self.__import_enrichment_into_db()
-        
-            if self.filename in self.available_correlations:
-                self.__import_correlation_into_db()
-
-            return self.yaml_object
-        else:
-            pass
     
     # Function imports multiple .yml files from the same directory into Django DB. 
     # Variable self.filename_contains specifies a part of the title of the file, which should be imported. 
@@ -96,11 +98,35 @@ class Data_Import:
     def validate_dependencies(self):
         pass
 
+    def __init__(self):
+        self.available_config_files = os.listdir(current_dir)
 
+        self.available_ucrs = []
+        self.available_groups = []
+        self.available_enrichments = []
+        self.available_correlations = []
+        self.available_ucrs_title_filename_dict = {}
+        self.available_groups_title_filename_dict = {}
+        self.available_enrichments_title_filename_dict = {}
+        self.available_correlations_title_filename_dict = {}
 
-if __name__ == "__main__":
-    ucr_import = Data_Import()
-    data = ucr_import.import_file(filename="UCR_AV_Symantec_01_Single_Alert.yml")
-    print(data)
-    mdata = ucr_import.import_multiple_files(filename_contains="_AV_")
-    print(mdata)
+        for config_file in self.available_config_files:
+            if config_file.startswith("UCR_"):
+                config_file_title = self.open_file(config_file)["title"]
+                self.available_ucrs.append(config_file)
+                self.available_ucrs_title_filename_dict[config_file_title] = config_file
+
+            elif config_file.startswith("GROUP_"):
+                config_file_title = self.open_file(config_file)["title"]
+                self.available_groups.append(config_file)
+                self.available_groups_title_filename_dict[config_file_title] = config_file
+
+            elif config_file.startswith("ENRICHMENT_"):
+                config_file_title = self.open_file(config_file)["title"]
+                self.available_enrichments.append(config_file)
+                self.available_enrichments_title_filename_dict[config_file_title] = config_file
+
+            elif config_file.startswith("CORRELATION_"):
+                config_file_title = self.open_file(config_file)["title"]
+                self.available_correlations.append(config_file)
+                self.available_correlations_title_filename_dict[config_file_title] = config_file
--- siem/backend/models.py
+++ siem/backend/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Group(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, primary_key=True, unique=True)
     description = models.CharField(max_length=256, default="New group", null=True, blank=True)
+    author = models.CharField(max_length=256, default="AYSOME IT Security", null=True, blank=True)
     index = models.CharField(max_length=64)
     sourcetype = models.CharField(max_length=64)
 
 class UCR(models.Model):
-    title = models.CharField(max_length=256, primary_key=True)
+    title = models.CharField(max_length=256, primary_key=True, unique=True)
     description = models.CharField(max_length=256, default="New use case rule", null=True, blank=True)
     author = models.CharField(max_length=256, default="AYSOME IT Security", null=True, blank=True)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
@@ -16,7 +17,7 @@ class UCR(models.Model):
     search_query = models.TextField()
 
 class Enrichment(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, primary_key=True, unique=True)
     description = models.CharField(max_length=256, default="New enrichment", null=True, blank=True)
     author = models.CharField(max_length=256, default="AYSOME IT Security", null=True, blank=True)
     required_fields = models.CharField(max_length=256)
@@ -25,12 +26,13 @@ class Enrichment(models.Model):
 
 
 class Correlation(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, primary_key=True, unique=True)
     description = models.CharField(max_length=256, default="New correlation", null=True, blank=True)
     author = models.CharField(max_length=256, default="AYSOME IT Security", null=True, blank=True)
 
 
 class Alert(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, primary_key=True, unique=True)
     description = models.CharField(max_length=256, default="New alert", null=True, blank=True)
+    author = models.CharField(max_length=256, default="AYSOME IT Security", null=True, blank=True)
     raw_event = models.TextField()
